3dc11116ccb46603bb5be2e42eacdd026f8e7700
Aivaras <as.savinis@gmail.com>
2021-10-09 15:08:23 +0300

    -

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,19 +1,34 @@
 class Tooltip {}
 
-class ProjectItem {}
+class ProjectItem {
+    constructor(id) {
+        this.id = id;
+    }
+}
 
 class ProjectList {
+    // Fields are concerted to properties before other constructor logic runs.
+    projects = [];
     constructor(type) {
         const prjItems = document.querySelectorAll(`#${type}-projects li`);
-        console.log(prjItems)
+        prjItems.forEach(item => {
+            this.projects.push(new ProjectItem(item.id));
+        });
+        console.log(this.projects)
     }
 }
 
 class App {
+
     static init () {
         const activeProjectList = new ProjectList('active');
         const finishedProjectList = new ProjectList('finished');
     }
+
+    static hello () {
+        console.log(1);
+    }
 }
 
 App.init();
+App.hello()
